60c5bc2da3555b06f2d29e7eeba8e504a7a4c75d
syntactical <greg.dutcher@gmail.com>
2016-10-11 16:59:53 -0400

    first commit

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Demo : MonoBehaviour
+{
+    public GameObject dataPoint;
+
+    private void Awake()
+    {
+
+        List<Dictionary<string, object>> data = DataReader.Read("example");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            foreach (KeyValuePair<string, object> kvp in data[i])
+            {
+                Debug.Log(kvp.Key + kvp.Value);
+            }
+
+            Instantiate(dataPoint,
+                new Vector3(Convert.ToInt32(data[i]["x"]), Convert.ToInt32(data[i]["y"]), Convert.ToInt32(data[i]["z"])),
+                Quaternion.identity);
+        }
+    }
+}
